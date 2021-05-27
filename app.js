0e4a9bbe111bd8b1d7282a07e259d3dc43abc19a
AdhamMohidat <>
2021-05-27 17:32:43 +0000

    add js file

---
+++ app.js
@@ -0,0 +1,18 @@
+ var nameWebSite ="google" ;
+
+var userName = prompt("Whats your name ?");
+alert("welcome  " + userName )
+
+
+var loverWebSite = prompt(" what kind of cars do you love?");
+
+
+if(loverWebSite === nameWebSite ){
+  document.write("<p> go to visit google </p>");
+}else if(loverWebSite === "youtibe"){
+  document.write("<p> go to visit youtube </p>");
+  else if(loverWebSite === "facebook"){
+  alert('please do not chose this web site ');}
+  else{ document.write('<img src="https://previews.123rf.com/images/hillway/hillway1801/hillway180100012/93530270-3d-vector-funny-gold-shiny-inscription-you-are-the-best-vector-illustration-.jpg"/> ');
+}
+
